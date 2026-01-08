6a5fe5f73f5d0ece676de41bccbb3c543ad8be7c
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2026-01-08 17:17:36 +0100

    feat: wip side tree implementation

--- src/sites/devops.ts
+++ src/sites/devops.ts
@@ -1,5 +1,31 @@
 import type { ReplacementSelectorSet, Site } from ".";
 
+const repositorySideTreeFolderImplementation: ReplacementSelectorSet = {
+	row: '.repos-file-explorer-tree-cell > div',
+	filename: 'a',
+	icon: '.fluent-icons-enabled > span',
+	// icon: '.fluent-icons-enabled:not(:has(.ms-Icon--ChevronRightMed, .ms-Icon--ChevronDownMed)) > span',
+	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
+		true,
+	isSubmodule: (_rowEl, _fileNameEl, _iconEl) =>
+		false, // TODO
+	isCollapsable: (rowEl, fileNameEl, iconEl) =>
+		true
+};
+
+const repositorySideTreeFileImplementation: ReplacementSelectorSet = {
+	row: '.repos-file-explorer-tree-cell:not(:has(>div))',
+	filename: 'a',
+	icon: '.fluent-icons-enabled:nth-child(2) > span',
+	// icon: '.fluent-icons-enabled:not(:has(.ms-Icon--ChevronRightMed, .ms-Icon--ChevronDownMed)) > span',
+	isDirectory: (_rowEl, _fileNameEl, iconEl) =>
+		false,
+	isSubmodule: (_rowEl, _fileNameEl, _iconEl) =>
+		false, // TODO
+	isCollapsable: (rowEl, fileNameEl, iconEl) =>
+		false
+};
+
 const repositoryMainImplementation: ReplacementSelectorSet = {
 	row: '.repos-files-hub-page tbody [aria-colindex=\'1\']',
 	filename: 'a',
@@ -10,11 +36,15 @@ const repositoryMainImplementation: ReplacementSelectorSet = {
 		false, // TODO
 	isCollapsable: (_rowEl, _fileNameEl, _iconEl) =>
 		false,
-}
+};
+
+
 
 export const devops: Site = {
 	domains: ['dev.azure.com'],
 	replacements: [
+		repositorySideTreeFolderImplementation,
+		repositorySideTreeFileImplementation,
 		repositoryMainImplementation
 	]
-}
+};
